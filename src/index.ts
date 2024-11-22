f5d47337baef3c8fb1c9d95a27c0101150273e96
junaidbinjaman <me@junaidbinjaman.com>
2024-11-22 20:20:20 +0600

    The ground is ready to practice webpack again

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { formData } from "./form";
-
-const form = document.querySelector('form')!;
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const data = formData(form);
-    console.log(data);
-});
