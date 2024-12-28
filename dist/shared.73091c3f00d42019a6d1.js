4a174fb2482df54467df61af57de4856183db5bf
junaidbinjaman <me@junaidbinjaman.com>
2024-12-28 09:23:13 +0600

    chor: recaping webpack knowledges - Deleted all the revious reference files. - .vscode file is not delleted to make like little easier. - The environment is clean for new practice. Notes: I will rewrite all the webpack related codes.

--- dist/shared.73091c3f00d42019a6d1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(self["webpackChunkwebpack_config_reference_main"] = self["webpackChunkwebpack_config_reference_main"] || []).push([["shared"],{},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["vendors"], () => (__webpack_exec__(543)));
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ }
-]);
